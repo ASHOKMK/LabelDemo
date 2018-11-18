afca81f0d9ea725a60bb2a0cf2fbf8f067f33c6b
ASHOKMK <ashokmkatakdhond7@gmail.com>
2018-11-18 16:22:42 +0530

    Update ViewController.swift
    roll no add

--- LabelDemo/ViewController.swift
+++ LabelDemo/ViewController.swift
@@ -15,7 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         let name = "shantaram"
         let middleName = "Bhaguji"
-        let lastname = "Kokate"
+        let lastname = "Kokate
+        let rolno  = "52"
     }
 
 
