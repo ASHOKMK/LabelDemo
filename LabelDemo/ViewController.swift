25191344758dfb3f709faa0440c37058a8d637dc
sidram <shantakokateit@gmail.com>
2018-11-18 16:09:05 +0530

    first commit

---
+++ LabelDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LabelDemo
+//
+//  Created by Ashok katakdhond on 18/11/18.
+//  Copyright © 2018 Ashok katakdhond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
